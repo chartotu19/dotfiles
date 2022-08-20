19d22e0e5beee6a2107df0f31f7976dfcf295de0
selvam palanimalai <selvam.palanimalai@gmail.com>
2022-08-20 10:14:25 -0400

    zsh: updating aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias vscode="/Applications/Visual\ Studio\ Code.app/Contents/MacOS/Electron ."
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
