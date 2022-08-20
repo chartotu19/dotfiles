44ef00e6e8e6b1f24c52ee67dfb8c7a453ed49d6
Selvam Palanimalai <selvam.palanimalai@gmail.com>
2022-08-20 09:18:40 -0400

    mac: updating list of brew packages to install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -23,4 +23,49 @@ fi
 
 brew install tig
 brew cask install visual-studio-code
+brew install kubectl
+brew install findutils
+brew install wget
+brew install tree
+brew install netcat
+brew install telnet
+brew install nvm
+brew install jq
+brew install argoproj/tap/argo
+brew install helm
+brew install mysql
+brew tap homebrew/cask
+brew cask install jumpcut
+brew install skaffold
+brew install envchain
+brew install stern
+brew install hey
+brew install nmap
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
+
+
+brew cleanup
 exit 0
