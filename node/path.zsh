6b5ab4d5479cc74355d1dfa2f29b976094f91f4c
selvam palanimalai <selvam.palanimalai@gmail.com>
2026-03-21 14:21:59 -0400

    Rewrite node install script for cross-platform NVM setup
    Replace curl-based install with robust NVM installer that works on both
    macOS and Linux. Add path.zsh for NVM environment configuration.

---
+++ node/path.zsh
@@ -0,0 +1,6 @@
+# NVM (Node Version Manager) configuration
+# https://github.com/nvm-sh/nvm
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
